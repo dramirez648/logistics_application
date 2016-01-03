4e1ab0c2c238ad5178e5d9de606eefb3d1adead5
dramirez648 <dramirez648@gmail.com>
2016-01-02 18:41:35 -0600

    exception handling
    logistics exception

---
+++ logisticsException.java
@@ -0,0 +1,10 @@
+package exception;
+
+public class LogisticsException extends Exception{
+
+  private static final long serialVersionUID = 1L;
+
+	public LogisticsException(String msg) {
+		super(msg);
+	}
+}
